7dbb3cfd531412251be463fcf36a9e7e92931109
Roman <romasemizon@gmail.com>
2019-05-29 08:48:26 +0300

    Corrected some values

--- FinalProject_LakesParadise_WebApplication/sql/5_crud_scripts.sql
+++ FinalProject_LakesParadise_WebApplication/sql/5_crud_scripts.sql
@@ -1,14 +1,14 @@
-insert into orders (user_id, home_id, date_start, date_end, status_pay) values (?, ?, ?, ?, ?)ж;
+insert into orders (user_id, home_id, date_start, date_end) values (?, ?, ?, ?, ?);
 
-select user_id, home_id, date_start, date_end, status_pay from orders where id = (?);
+select user_id, home_id, date_start, date_end from orders where id = (?);
 
-update orders set user_id = ?, home_id = ?, date_start = ?, date_end = ?, status_pay = ? where id = ?;
+update orders set user_id = ?, home_id = ?, date_start = ?, date_end = ? where id = ?;
 
-INSERT INTO homesteads (title, price, description, people_number, rating, number_of_voted_users, owner_id) values (?, ?, ?, ?, ?, ?, ?);
+INSERT INTO homesteads (title, price, description, people_number, owner_id) values (?, ?, ?, ?, ?, ?, ?);
 
-SELECT title, people_number, price, description, rating, number_of_voted_users, owner_id FROM homesteads WHERE id = (?);
+SELECT title, people_number, price, description, owner_id FROM homesteads WHERE id = (?);
 
-UPDATE homesteads SET title = ?, people_number = ?, price = ?, description = ?, rating = ?, number_of_voted_users = ?, owner_id = ? where id = ?;
+UPDATE homesteads SET title = ?, people_number = ?, price = ?, description = ?, owner_id = ? where id = ?;
 
 select id, text, user_name, date_of_comment from reviews where home_id = (?);
 
